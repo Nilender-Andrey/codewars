1b5e39c4feef21528de150f2f473021406d9f025
Nilender-Andrey <nilenderav@mail.ru>
2021-12-22 20:38:28 +0400

    add solution

---
+++ index.js
@@ -0,0 +1,16 @@
+//Power of two
+
+/*Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two.
+ From the corresponding Wikipedia entry:
+a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+You may assume the input is always valid.*/
+/* 
+function isPowerOfTwo(n) {
+  return Math.log2(n) % 1 === 0;
+}
+
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(4096)); // true
+console.log(isPowerOfTwo(5)); // false
+ */ //Не Мое решение
