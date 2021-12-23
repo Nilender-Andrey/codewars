1e64608b874d31e947e955b143146e8f522c365a
Nilender-Andrey <nilenderav@mail.ru>
2021-12-23 21:40:29 +0400

    add solution

--- index.js
+++ index.js
@@ -84,3 +84,22 @@ console.log(
   ),
 ); // 3
  */
+
+//*Vowel one
+/* 
+vowelOne
+Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+ */
+
+function vowelOne(s) {
+  return s
+    .toLowerCase()
+    .split('')
+    .map((s) => ('aeuoi'.includes(s) ? 1 : 0))
+    .join('');
+}
+
+console.log(vowelOne('vowelOne')); // "01010101"
+console.log(vowelOne('123, arou')); // "000001011"
