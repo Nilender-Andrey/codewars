072a1ef2ee2537df85f1f436cca248466999a56f
Nilender-Andrey <nilenderav@mail.ru>
2021-12-23 20:54:00 +0400

    add solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Power of two
+//*Power of two
 
 /*Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two.
  From the corresponding Wikipedia entry:
@@ -14,3 +14,30 @@ console.log(isPowerOfTwo(2)); // true
 console.log(isPowerOfTwo(4096)); // true
 console.log(isPowerOfTwo(5)); // false
  */ //Не Мое решение
+
+//*String Reordering
+
+/*The input will be an array of dictionaries.
+
+Return the values as a string-seperated sentence in the order of their keys' integer equivalent (increasing order).
+
+The keys are not reoccurring and their range is -999 < key < 999. The dictionaries' keys & values will always be strings and will always not be empty.  */
+/* 
+let List = [
+  { 4: 'dog' },
+  { 2: 'took' },
+  { 3: 'his' },
+  { '-2': 'Vatsan' },
+  { 5: 'for' },
+  { 6: 'a' },
+  { 12: 'spin' },
+];
+
+function sentence(List) {
+  return List.sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
+    .map((item) => Object.values(item))
+    .join(' ');
+}
+
+console.log(sentence(List)); //'Vatsan took his dog for a spin'
+ */
