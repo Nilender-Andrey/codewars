4bb5b4ca14f917b736bb3443d801faab76491033
Nilender-Andrey <nilenderav@mail.ru>
2022-01-08 18:47:11 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -79,3 +79,20 @@ function randomArr(min, max, key) {
 }
 
 console.log(getCard()); */
+/* 
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
+You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+//* Bit Counting
+/* 
+const countBits = (n) =>
+  n
+    .toString(2)
+    .split('')
+    .filter((item) => item === '1').length;
+
+console.log(countBits(7)); // 3
+ */
