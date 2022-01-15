80aacfdd47f5d79506205002e8859888facefdf9
Nilender-Andrey <nilenderav@mail.ru>
2022-01-15 15:44:27 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ The data is given in an array as such:
 [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
 Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
  */
-/* 
+/*
 const data1 = [
   1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
   1, 0, 1, 0, 1, 0,
@@ -43,7 +43,7 @@ console.log(dataReverse(data2)); //  [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 
  */
 
 //* Bingo Card
-/* 
+/*
 function getCard() {
   const bingo = [
     { B: [1, 15] },
@@ -79,15 +79,15 @@ function randomArr(min, max, key) {
 }
 
 console.log(getCard()); */
-/* 
-Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
 You can guarantee that input is non-negative.
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 */
 
 //* Bit Counting
-/* 
+/*
 const countBits = (n) =>
   n
     .toString(2)
@@ -96,7 +96,7 @@ const countBits = (n) =>
 
 console.log(countBits(7)); // 3
  */
-
+/*
 function whatTimeIsIt(angle) {
   let hour = Math.floor(angle / 30);
   hour = hour === 0 ? 12 : hour;
@@ -105,3 +105,36 @@ function whatTimeIsIt(angle) {
 }
 
 console.log(whatTimeIsIt(271)); //09:02
+ */
+
+//* Is a number prime?
+
+/* 
+Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. 
+Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow. 
+
+
+
+function isPrime(num) {
+  if (num <= 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(0));
+console.log(isPrime(1));
+console.log(isPrime(73));
+console.log(isPrime(75));
+console.log(isPrime(-1));
+console.log(isPrime(4));
+*/
