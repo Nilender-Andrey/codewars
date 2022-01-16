a7a53f7a80a1141bd7625e3f4b6ce106b343c7fd
Nilender-Andrey <nilenderav@mail.ru>
2022-01-16 21:03:50 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -158,6 +158,7 @@ If your total is more than or equal to (win), return 'Winner!'. Else return 'Los
 All inputs will be in the correct format. Strings on tickets are not always the same length.
 */
 
+/* 
 function bingo(ticket, win) {
   const result = ticket.reduce((sum, item) => {
     const char = item[0].split('').find((c) => c.charCodeAt(0) === item[1]);
@@ -200,3 +201,36 @@ console.log(
     2,
   ),
 ); // 'Loser!'
+ */
+
+//* WeIrD StRiNg CaSe
+/* 
+Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. 
+The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). 
+Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+toWeirdCase( "String" );//=> returns "StRiNg"
+toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+*/
+
+function toWeirdCase(string) {
+  let i = 0;
+
+  return string.split('').reduce((result, item) => {
+    if (item === ' ') {
+      result += ' ';
+      i = 0;
+      return result;
+    }
+    const char = i % 2 === 0 ? item.toUpperCase() : item.toLowerCase();
+    i++;
+    return (result += char);
+  }, '');
+}
+
+console.log(toWeirdCase('This')); // 'ThIs'
+console.log(toWeirdCase('is')); //  'Is'
+console.log(toWeirdCase('This is a test')); // 'ThIs Is A TeSt'
