57abf2c5a75a98e5026a6232561cd40398e0f372
Nilender-Andrey <nilenderav@mail.ru>
2022-01-18 22:05:57 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -141,3 +141,24 @@ console.log(
   }),
 );
  */
+
+//* Get decimal part of the given number
+/* 
+Write a function that returns only the decimal part of the given number.
+
+You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+
+Examples
+getDecimal(2.4)  === 0.4
+getDecimal(-0.2) === 0.2
+*/
+/* 
+function getDecimal(num) {
+  const i = (num + '').split('.');
+  return i.length === 1 ? 0 : +`0.${i[1]}`;
+}
+
+console.log(getDecimal(0.2)); //0.2
+console.log(getDecimal(4.5)); //0.5
+console.log(getDecimal(-10)); //0
+ */
