3ad211f183458b1f090a8195dff411baac036572
Nilender-Andrey <nilenderav@mail.ru>
2022-03-09 18:54:24 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -740,3 +740,31 @@ console.log(sortByBit([7, 6, 15, 8])); //[8, 6, 7, 15]
 console.log(sortByBit([3, 8, 3, 6, 5, 7, 9, 1])); // [1, 8, 3, 3, 5, 6, 9, 7]
 console.log(sortByBit([9, 4, 5, 3, 5, 7, 2, 56, 8, 2, 6, 8, 0])); // [0, 2, 2, 4, 8, 8, 3, 5, 5, 6, 9, 7, 56]
  */
+
+//* Nuclear Missile Manager
+/* 
+Our nuclear missile manager system is coded in Node.js. 
+We are currently testing the system and it seems that the launchAll function does not work as expected. 
+It should launch 5 missiles each 1 second apart. 
+The current code tries to launch the missile #5 five times...
+
+Can you fix this for us? You know, it's pretty critical code...
+
+Note: There are 5 missiles labeled i which is a number in {0, 1, 2, 3, 4}.
+ The missile i should be launched after i seconds.
+*/
+/* 
+function launchAll(launchMissile) {
+  for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+      launchMissile(i);
+    }, i * 1000);
+  }
+}
+
+const launchMissile = (i) => {
+  console.log(i);
+};
+
+console.log(launchAll(launchMissile));
+ */
