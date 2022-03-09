ec5af74d26f65f4035f86678900ebcef511a52c1
Nilender-Andrey <nilenderav@mail.ru>
2022-03-09 19:58:09 +0400

    feat: add solution

--- index.js
+++ index.js
@@ -783,7 +783,7 @@ result = 2 (1 red pair + 1 blue pair)
 input = ["red", "red", "red", "red", "red", "red"]
 result = 3 (3 red pairs) 
 */
-
+/* 
 function numberOfPairs(gloves) {
   return Array.from(new Set(gloves)).reduce((sum, item) => {
     return (sum += Math.trunc(gloves.filter((i) => i === item).length / 2));
@@ -792,3 +792,70 @@ function numberOfPairs(gloves) {
 
 console.log(numberOfPairs(['red', 'green', 'red', 'blue', 'blue'])); //2
 console.log(numberOfPairs(['red', 'red', 'red', 'red', 'red', 'red'])); // 3
+ */
+
+//* Length of missing array
+/* 
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+
+You have to write a method, that return the length of the missing array.
+
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+If the array of arrays is null/nil or empty, the method should return 0.
+
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have created other katas. Have a look if you like coding and challenges.
+*/
+/* 
+function getLengthOfMissingArray(arrayOfArrays) {
+  if (
+    arrayOfArrays == null ||
+    !arrayOfArrays.length ||
+    checkNull(arrayOfArrays)
+  )
+    return 0;
+
+  const sortArr = arrayOfArrays.sort((a, b) => a.length - b.length);
+
+  const filterArr = sortArr.filter((item, index, arr) => {
+    if (arr[index + 1] && item.length + 1 !== arr[index + 1].length) {
+      return true;
+    }
+    return false;
+  })[0];
+
+  return filterArr.length + 1;
+}
+
+function checkNull(arr) {
+  return arr.filter((item) => item == null || item.length === 0).length;
+}
+
+console.log(
+  getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]),
+); // 3
+console.log(
+  getLengthOfMissingArray([[5, 2, 9], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]),
+); // 2
+console.log(getLengthOfMissingArray([[null], [null, null, null]])); // 2;
+console.log(
+  getLengthOfMissingArray([
+    ['a', 'a', 'a'],
+    ['a', 'a'],
+    ['a', 'a', 'a', 'a'],
+    ['a'],
+    ['a', 'a', 'a', 'a', 'a', 'a'],
+  ]),
+); // 5
+
+console.log(getLengthOfMissingArray([])); // 0
+ */
